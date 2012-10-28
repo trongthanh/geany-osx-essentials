7ae8be5079fc933a9480dcfcdadb622c2cc501b4
Thanh Tran <trongthanh@gmail.com>
2012-10-28 18:54:24 +0800

    - add PATH environment so that Geany can find and execute default apps from make/compile menu; - fix typo in bundle

--- launcher.sh
+++ launcher.sh
@@ -33,6 +33,7 @@ export GTK2_RC_FILES="$bundle_etc/gtk-2.0/gtkrc"
 export GTK_IM_MODULE_FILE="$bundle_etc/gtk-2.0/gtk.immodules"
 export GDK_PIXBUF_MODULE_FILE="$bundle_etc/gtk-2.0/gdk-pixbuf.loaders"
 export PANGO_RC_FILE="$bundle_etc/pango/pangorc"
+export PATH="/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/git/bin"
 
 APP=name
 I18NDIR="$bundle_data/locale"
